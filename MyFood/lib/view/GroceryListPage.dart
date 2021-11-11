d9d1ea8eff8d505392f6a0abcb240435c15eac0e
Nelson Tejeda <nelsonjesustejeda@gmail.com>
2021-11-10 21:08:07 -0500

    added basic functionality to groceryList

--- MyFood/lib/view/GroceryListPage.dart
+++ MyFood/lib/view/GroceryListPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class GroceryListPage extends StatefulWidget {
   const GroceryListPage({Key? key}) : super(key: key);
@@ -8,17 +10,67 @@ class GroceryListPage extends StatefulWidget {
 }
 
 class _GroceryListPageState extends State<GroceryListPage> {
+  List<String> gl = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          child: Text("Return to login"),
-          onPressed: () {
-            Navigator.pop(context);
+        child: FutureBuilder(
+          future: getGroceryList(),
+          builder: (context,snapshot){
+            if(snapshot.hasData){
+              List<QueryDocumentSnapshot> data =
+              snapshot.data as List<QueryDocumentSnapshot>;
+              if(data.isEmpty){
+                return Center(
+                  child: Text("You have nothing in your groceryList"),
+                );
+              }
+              return ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context,index){
+                  return Container(
+                    child: ListTile(
+                      title: Text(data[index]["name"]),
+                      trailing: Text(data[index]["place"]),
+                      subtitle: Text("Date:" + data[index]["date"]),
+                      onLongPress: () async{
+                        await deleteItem(data[index].id);
+                        setState(() {});
+                      },
+                    ),
+                  );
+                },
+              );
+            }
+            else{
+              return CircularProgressIndicator();
+            }
           },
-        ),
+        )
       ),
     );
   }
+}
+
+Future<List<QueryDocumentSnapshot>> getGroceryList() async {
+  List<QueryDocumentSnapshot> arr = [];
+  await FirebaseFirestore.instance
+  .collection('Users')
+  .doc("0WPVnyaGEGOZDM8koHtX4gLnR443").collection("food")
+  .get()
+  .then((QuerySnapshot querySnapshot) {
+    querySnapshot.docs.forEach((doc) {
+      arr.add(doc);
+    });
+  });
+  return arr;
+}
+
+Future<void> deleteItem(String id) {
+  return FirebaseFirestore.instance.collection("Users")
+      .doc('0WPVnyaGEGOZDM8koHtX4gLnR443').collection("food").doc(id)
+      .delete()
+      .then((value) => print("Item Deleted"))
+      .catchError((error) => print("Failed to delete item: $error"));
 }
