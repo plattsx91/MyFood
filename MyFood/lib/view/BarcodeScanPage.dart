65d6a08b849bd72ea2cb616d46eb87e66f98e63f
ovil101 <rlioy1211@gmail.com>
2021-11-18 11:17:56 -0500

    Moved getProducts() to a seperate file

--- MyFood/lib/view/BarcodeScanPage.dart
+++ MyFood/lib/view/BarcodeScanPage.dart
@@ -3,25 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:barcode_scan2/barcode_scan2.dart';
 import 'package:flutter/services.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
+import 'OpenFoodFactsAPI.dart';
 
 class BarcodeScanPage extends StatefulWidget {
   @override
   BarcodeScanPageState createState() {
     return new BarcodeScanPageState();
-  }
-}
-
-// gets product name based on barcode
-Future<Product> getProduct(var barcode) async {
-  ProductQueryConfiguration configuration = ProductQueryConfiguration(barcode,
-      language: OpenFoodFactsLanguage.ENGLISH, fields: [ProductField.ALL]);
-  ProductResult result = await OpenFoodAPIClient.getProduct(configuration);
-
-  if (result.status == 1) {
-    return result.product!;
-  } else {
-    throw Exception('product not found, please insert data for ' +
-        barcode); // have to do this eventually
   }
 }
 
